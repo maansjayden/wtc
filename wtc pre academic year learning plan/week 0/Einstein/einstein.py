4b81ebaf6ae4cf62b79d28186afff49d4852c71c
maansjayden <maansjayden56@gmail.com>
2025-07-09 12:57:49 +0200

    Add files via upload
    week 0 projects

---
+++ wtc pre academic year learning plan/week 0/Einstein/einstein.py
@@ -0,0 +1,5 @@
+mass = int(input("m: "))
+c = 300000000
+E = mass * (c ** 2)
+
+print("E: ", E)
---
+++ wtc pre academic year learning plan/week 0/Making Faces/faces.py
@@ -0,0 +1,8 @@
+def main():
+    input_str = input("Enter input: ")
+    input_str = input_str.replace(":)","🙂")
+    input_str = input_str.replace(":(","🙁")
+    print(input_str)
+main()
+
+
